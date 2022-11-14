f270214471d2d88da8574813b3cae9e778e08a44
Felipe Ramon <rammonfelip@gmail.com>
2022-11-14 01:18:07 -0300

    Ajustando as rotas da aplicação.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::name('api.')->prefix('/api')->group(function() {
+    Route::get('company', [ApiController::class, 'getCotationInfo'])->name('company');
+    Route::get('last-price', [ApiController::class, 'getLatestPrice'])->name('latestPrice');
+    Route::get('company/{symbol}/', [ApiController::class, 'getCompanyInfo'])->name('company-info');
+    Route::get('company/{symbol}/quote/{field?}', [ApiController::class, 'getQuoteInfo'])->name('company-quote');
 });
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ApiController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,11 +9,6 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::name('api.')->prefix('/api')->group(function() {
-        Route::get('company', [ApiController::class, 'getCotationInfo'])->name('company');
-        Route::get('last-price', [ApiController::class, 'getLatestPrice'])->name('latestPrice');
-    });
 });
 
 require __DIR__.'/auth.php';
